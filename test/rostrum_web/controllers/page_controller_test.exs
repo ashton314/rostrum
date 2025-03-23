298c472c36891fe688bfa17b0b701344fa271c4f
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-03-23 16:48:56 -0600

    Burn down to only 43 remaining failures

--- test/rostrum_web/controllers/page_controller_test.exs
+++ test/rostrum_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RostrumWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Elegant digital programs for your worship service"
   end
 end
--- test/rostrum_web/live/user_registration_live_test.exs
+++ test/rostrum_web/live/user_registration_live_test.exs
@@ -17,7 +17,7 @@ defmodule RostrumWeb.UserRegistrationLiveTest do
         conn
         |> log_in_user(user_fixture())
         |> live(~p"/users/register")
-        |> follow_redirect(conn, "/")
+        |> follow_redirect(conn, "/dash")
 
       assert {:ok, _conn} = result
     end
@@ -45,10 +45,10 @@ defmodule RostrumWeb.UserRegistrationLiveTest do
       render_submit(form)
       conn = follow_trigger_action(form, conn)
 
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/dash"
 
       # Now do a logged in request and assert on the menu
-      conn = get(conn, "/")
+      conn = get(conn, "/dash")
       response = html_response(conn, 200)
       assert response =~ email
       assert response =~ "Settings"
